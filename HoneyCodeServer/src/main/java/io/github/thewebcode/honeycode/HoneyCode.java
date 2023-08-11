9e885998c4a6bc73735ca92f6db6150eef67ec44
TheWebcode <webcode_mail@yahoo.com>
2023-08-11 12:04:18 +0200

    Initial Commit

---
+++ HoneyCodeServer/src/main/java/io/github/thewebcode/honeycode/HoneyCode.java
@@ -0,0 +1,16 @@
+package io.github.thewebcode.honeycode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HoneyCode extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
